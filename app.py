64a9c8ad6adaf2e402f06fa65c054db87116c4c3
Nhi TRAN <nntran@users.noreply.github.com>
2019-09-26 11:38:26 +0200

    Show container ID

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
+hostname = socket.gethostname()
 
 @app.route('/')
 def show_picture():
     pic_file = os.path.join('static', 'photo.jpg')
-    return render_template("index.html", picture = pic_file)
+    return render_template("index.html", picture=pic_file, hostname=hostname)
  
 if __name__=="__main__":
-    app.run(debug=True)
+    app.logger.info('Container ID: %s', hostname)
+    app.run(debug=True, threaded=True)
